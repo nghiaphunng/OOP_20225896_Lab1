15cbf1455513eac162ceb9f6203748f11e5b00c3
nghiaphunng <duynghia135z@gmail.com>
2024-10-28 01:57:13 +0700

    update

---
+++ Lab01/very_first_java_programs/HelloNameDialog.java
@@ -0,0 +1,12 @@
+package javacore_lab.week1.very_first_java_programs;
+
+import javax.swing.*;
+
+public class HelloNameDialog {
+    public static void main(String[] args) {
+        String result;
+        result = JOptionPane.showInputDialog("Please enter your name:");
+        JOptionPane.showMessageDialog(null, "Hi " + result + "!");
+        System.exit(0);
+    }
+}
