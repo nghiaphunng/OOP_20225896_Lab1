d5ec85e26156b201e4f7809bfd2690d86841d181
nghiaphunng <duynghia135z@gmail.com>
2024-11-29 15:01:20 +0700

    update folder clean

--- src/javacore_lab/week1/very_first_java_programs/HelloWorld.java
+++
@@ -1,8 +0,0 @@
-package javacore_lab.week1.very_first_java_programs;
-
-public class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("Xin chao \n cac ban!");
-        System.out.println("Hello \t World!");
-    }
-}
