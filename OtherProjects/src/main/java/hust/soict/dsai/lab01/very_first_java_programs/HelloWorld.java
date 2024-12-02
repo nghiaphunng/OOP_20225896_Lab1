eab5b6284a6b9778935993a9fc894d1caf0a0982
nghiaphunng <duynghia135z@gmail.com>
2024-12-02 23:18:00 +0700

    update

--- OtherProjects/src/main/java/hust/soict/dsai/lab01/very_first_java_programs/HelloWorld.java
+++
@@ -1,8 +0,0 @@
-package hust.soict.dsai.lab01.very_first_java_programs;
-
-public class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("Xin chao \n cac ban!");
-        System.out.println("Hello \t World!");
-    }
-}
